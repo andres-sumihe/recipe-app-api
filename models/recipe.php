0924270d2e543d8013d51c02799066b6eb1e4345
andres-sumihe <672018136@student.uksw.edu>
2021-06-30 23:35:27 +0900

    Testing Hapus

--- models/recipe.php
+++ models/recipe.php
@@ -49,14 +49,6 @@
         $this->groceries=htmlspecialchars(strip_tags($this->groceries));
         $this->coocking_steps=htmlspecialchars(strip_tags($this->coocking_steps));
         $this->created=htmlspecialchars(strip_tags($this->created));
-      
-        $stmt->bindParam(":category_id", $this->category_id);
-        $stmt->bindParam(":recipe_name", $this->recipe_name);
-        $stmt->bindParam(":picture", $this->picture);
-        $stmt->bindParam(":description", $this->description);
-        $stmt->bindParam(":groceries", $this->groceries);
-        $stmt->bindParam(":coocking_steps", $this->coocking_steps);
-        $stmt->bindParam(":created", $this->created);
         
       
         // execute query
